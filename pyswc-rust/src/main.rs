9b6004251898611033b90c4dcc4a5091ba26232a
Chuck Rai <cprai@sfu.ca>
2021-12-15 22:23:25 -0500

    Create js minifier

--- pyswc-rust/src/main.rs
+++ pyswc-rust/src/main.rs
@@ -1,9 +1,12 @@
-use std::{path::Path, sync::Arc};
-use swc::{self, config::Options};
-use swc_common::{
-    errors::{ColorConfig, Handler},
-    SourceMap,
-};
+use std::path::Path;
+use std::sync::Arc;
+use swc::Compiler;
+use swc::config::{JsMinifyOptions, JsMinifyFormatOptions};
+use swc::config::util::BoolOrObject;
+use swc_common::SourceMap;
+use swc_common::errors::{ColorConfig, Handler};
+use swc_ecma_minifier::option::terser::TerserEcmaVersion;
+
 
 fn main() {
     let cm = Arc::<SourceMap>::default();
@@ -13,17 +16,50 @@ fn main() {
         false,
         Some(cm.clone()),
     ));
-    let c = swc::Compiler::new(cm.clone());
+    let c = Compiler::new(cm.clone());
 
     let fm = cm
         .load_file(Path::new("foo.js"))
         .expect("failed to load file");
 
-    let transformed = c.process_js_file(
+    let transformed = c.minify(
         fm,
         &handler,
-        &Options {
-            ..Default::default()
+        &JsMinifyOptions {
+            compress: true.into(),
+            mangle: true.into(),
+            format: JsMinifyFormatOptions {
+                ascii_only: false,
+                beautify: false,
+                braces: true,
+                comments: false.into(),
+                ecma: 5,
+                indent_level: 4,
+                indent_start: false,
+                inline_script: false,
+                keep_numbers: true,
+                keep_quoted_props: true,
+                max_line_len: BoolOrObject::Obj(80),
+                preamble: String::from(""),
+                quote_keys: true,
+                quote_style: 3,
+                preserve_annotations: true,
+                safari10: false,
+                semicolons: true,
+                shebang: false,
+                webkit: false,
+                wrap_iife: true,
+                wrap_func_args: true,
+            },
+            ecma: TerserEcmaVersion::Num(5),
+            keep_classnames: true,
+            keep_fnames: true,
+            module: false,
+            safari10: false,
+            toplevel: false,
+            source_map: false.into(),
+            output_path: None,
+            inline_sources_content: true,
         },
     )
     .expect("failed to process file");
